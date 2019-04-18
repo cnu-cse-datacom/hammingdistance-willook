09bded45426fe51dc674881d009de3f539ccae0b
willook <willook@naver.com>
2019-04-18 17:58:16 +0900

    Update DC02_06_201502096_이종화.py

--- DC02_06_201502096_이종화.py
+++ DC02_06_201502096_이종화.py
@@ -1,16 +1,18 @@
 import numpy as np
 import pandas as pd
 import random
+from hamming_parctice import hamming
 
 def hamming(a,b):
     return len([i for i in filter(lambda x: x[0] != x[1], zip(a,b))])
 
+
 def main():
     df = pd.read_csv('sample.csv',names=['word', bin])
-
-    min = 1000000
+    print()
+    min = -1
     count  = 1
-    length = df.size//2
+    length = df.shape[0]
     #length = 10
     #print(df.size)
     for i in range(length-1):
@@ -18,11 +20,13 @@ def main():
             #print(i,j)
             hd = hamming(df.iloc[i,1],df.iloc[j,1])
             print(count,"(",df.iloc[i,0],df.iloc[j,0],")hamming_distance:",hd)
-            if min > hd:
+            if min == -1 or min > hd:
                 min = hd;
             count += 1
     print("min hamming distance", min)    
     
   
+
+    
 if __name__ == "__main__":
     main()
