66cdd1d284061a0a2ebd5b16ede4107e9cf8500e
willook <willook@naver.com>
2019-04-18 17:28:32 +0900

    Add files via upload

---
+++ DC02_06_201502096_이종화.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+import random
+
+def hamming(a,b):
+    return len([i for i in filter(lambda x: x[0] != x[1], zip(a,b))])
+
+def main():
+    df = pd.read_csv('sample.csv',names=['word', bin])
+
+    min = 1000000
+    count  = 1
+    length = df.size//2
+    #length = 10
+    #print(df.size)
+    for i in range(length-1):
+        for j in range(i+1, length):
+            #print(i,j)
+            hd = hamming(df.iloc[i,1],df.iloc[j,1])
+            print(count,"(",df.iloc[i,0],df.iloc[j,0],")hamming_distance:",hd)
+            if min > hd:
+                min = hd;
+            count += 1
+    print("min hamming distance", min)    
+    
+  
+if __name__ == "__main__":
+    main()
